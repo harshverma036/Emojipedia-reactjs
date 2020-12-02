9a51bb74d348177759e5f07dc10332ab76e09b5f
Harsh Verma <harshverma0362@gmail.com>
2020-12-02 10:29:52 +0530

    Completed EmojiPedia Project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './bootstrap.min.css';
+import './style.css';
+import App from './app';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <div className="container pt-5">
     <App />
-  </React.StrictMode>,
+  </div>
+  ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
